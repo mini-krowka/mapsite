36d34776691380180c067a70703c94f8b92f9155
mini-krowka <tophbeifong123@protonmail.com>
2025-10-12 00:00:11 +0300

    Изменен файл по дате

--- js/data.js
+++ js/data.js
@@ -62,7 +62,7 @@ window.kmlFiles = [
     { name: "15.09.25", path: "kml/ControlZones/Control_25_09_15.kml" },
     { name: "19.09.25", path: "kml/ControlZones/Control_25_09_19.kml" },
     { name: "23.09.25", path: "kml/ControlZones/Control_25_09_23.kml" },
-    { name: "10.10.25", path: "kml/PermanentObjects/ИЗМЕНЕНИЯ.kml" },
+    { name: "10.10.25", path: "kml/PermanentObjects/Progress_25_10_09.geojson" },
 ];
 
 // Постоянные слои
@@ -95,6 +95,7 @@ window.cities = [
     { name: { ru: "Гуляйполе", en: "Gulyaypole" }, lat: 47.66336,  lng:  36.2587   },
     { name: { ru: "Орехов",    en: "Orekhov"    }, lat: 47.5675,   lng:  35.78845  },
 ];
+
 
 
 
