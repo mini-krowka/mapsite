7a6d88929806023aaada57aacca08444bb182ecc
mini-krowka <tophbeifong123@protonmail.com>
2025-10-13 12:09:37 +0300

    Удалена дата с кмл мультиполигоном

--- js/data.js
+++ js/data.js
@@ -61,9 +61,8 @@ window.kmlFiles = [
     { name: "11.09.25", path: "kml/ControlZones/Control_25_09_11.kml" },
     { name: "15.09.25", path: "kml/ControlZones/Control_25_09_15.kml" },
     { name: "19.09.25", path: "kml/ControlZones/Control_25_09_19.kml" },
-    { name: "23.09.25", path: "kml/ControlZones/Control_25_09_23.kml" },
-    { name: "10.10.25", path: "kml/PermanentObjects/Progress_25_10_09.geojson" },
-];
+    { name: "23.09.25", path: "kml/ControlZones/Control_25_09_23.kml" }
+   ];
 
 // Постоянные слои
 window.permanentLayers = [
@@ -95,6 +94,7 @@ window.cities = [
     { name: { ru: "Гуляйполе", en: "Gulyaypole" }, lat: 47.66336,  lng:  36.2587   },
     { name: { ru: "Орехов",    en: "Orekhov"    }, lat: 47.5675,   lng:  35.78845  },
 ];
+
 
 
 
