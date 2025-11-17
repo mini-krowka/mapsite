9995d8fa3a0735f5e755d6034f478b71702a97bd
mini-krowka <tophbeifong123@protonmail.com>
2025-11-18 02:19:14 +0300

    Изменен файл

--- js/data.js
+++ js/data.js
@@ -321,12 +321,13 @@ window.cities = [
 
 
 // Управление точками
-window.pointsKmlPath = "kml/Geolocations/Сurrent.kml"; // Путь к файлу с точками
+window.pointsKmlPath = "kml/Geolocations/Current.kml"; // Путь к файлу с точками
 window.pointLayers = [];
 window.pointsDateRange = {
     start: new Date(2025, 10, 9), // 10 ноября
     end: new Date(2025, 10, 16)   // 17 ноября
 };
+
 
 
 
