5361d7c38ead0e087cc5990f885ccdfd148454b0
mini-krowka <tophbeifong123@protonmail.com>
2025-12-02 20:43:02 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -469,12 +469,12 @@ window.kmlStyles = {
             color: '#999999', // Красная обводка для видимости
             weight: 2, // Тонкая линия
             fillColor: '#999999', // Красная заливка
-            fillOpacity: 0.3, // Низкая непрозрачность
+            fillOpacity: 0.2, // Низкая непрозрачность
             interactive: false
         },
         polyline: {
             color: '#999999', // Синие линии
-            weight: 4, // Толстая линия
+            weight: 2, // Толстая линия
             opacity: 1,
             interactive: false
         }
@@ -2557,6 +2557,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
 
 
 
