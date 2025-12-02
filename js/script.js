11bb4792230e76dc3cf1e900c0736b9dcf0c4d1b
mini-krowka <tophbeifong123@protonmail.com>
2025-12-02 23:33:14 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -466,14 +466,14 @@ window.kmlStyleModes = {
 window.kmlStyles = {
     [window.kmlStyleModes.STYLE_MG]: {
         polygon: {
-            color: '#999999', // Красная обводка для видимости
+            color: '#ffffff', // Красная обводка для видимости
             weight: 1, // Тонкая линия
             fillColor: '#999999', // Красная заливка
-            fillOpacity: 0.1, // Низкая непрозрачность
+            fillOpacity: 0.2, // Низкая непрозрачность
             interactive: false
         },
         polyline: {
-            color: '#999999', // Синие линии
+            color: '#ffffff', // Синие линии
             weight: 1, // Толстая линия
             opacity: 1,
             interactive: false
@@ -2557,6 +2557,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
 
 
 
