57c675dac9c8e4df7096aa03a497224224290a16
mini-krowka <tophbeifong123@protonmail.com>
2025-12-22 01:00:33 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -362,12 +362,13 @@ window.cities = [
 // Управление точками
 // Путь к файлу с точками
 // window.pointsKmlPaths = ["kml/Geolocations/2025 год ноябрь - декабрь.kml",
-                          "kml/Geolocations/2025 .kml"];
+//                          "kml/Geolocations/2025 .kml"];
 // window.pointLayers = [];
 // window.pointsDateRange = {
 //    start: null,
 //    end: null
 // };
+
 
 
 
