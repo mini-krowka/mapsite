64da58e20c5e52f5ef296b5cf06a1be72f83e99b
mini-krowka <tophbeifong123@protonmail.com>
2026-02-02 16:29:53 +0300

    Исправлены пути файлов

--- js/data.js
+++ js/data.js
@@ -370,8 +370,8 @@ window.kmlFiles = [
 
 
 	{name: "29.01.26", paths: [
-        	                    "kml/ControlZones/Control_26_01_13.kml",
-								"kml/FrontLine/FrontLine_26_01_13.kml",
+        	                    "kml/ControlZones/Control_26_01_29.kml",
+								"kml/FrontLine/FrontLine_26_01_29.kml",
                                 "kml/Progress/Progress_26_01_29.kml"] },
 	{name: "02.02.26", paths: [
         	                    "kml/ControlZones/Control_26_02_02.kml",
@@ -455,6 +455,7 @@ window.milequipLayers = [];
 window.attacksOnUaKmlPaths = ["kml/Geolocations/osint_alarm_2025.kml",
                               "kml/Geolocations/osint_alarm_2026.kml"];
 window.attacksOnUaLayers = [];
+
 
 
 
