d0ba31f0b16ad7f75f513c157e2bf0827fde93ed
mini-krowka <tophbeifong123@protonmail.com>
2026-02-02 16:25:09 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -398,7 +398,7 @@ window.permanentLayers = [
 	// { name: "Херсонская область",        path: "kml/PermanentObjects/Херсонская область.kml" },
 	{ name: "Области",        path: "kml/PermanentObjects/Области.kml" },
     // Текущая ЛБС
-     { name: "Current_frontline",      path: "kml/FrontLine/FrontLine_25_12_24.kml" },
+     { name: "Current_frontline",      path: "kml/FrontLine/FrontLine_26_02_02.kml" },
 	
     // Граница ЛДНР без Ростовской области
     // { name: "LDPR",                   path: "kml/PermanentObjects/LDPR_line3.kml" },
@@ -455,6 +455,7 @@ window.milequipLayers = [];
 window.attacksOnUaKmlPaths = ["kml/Geolocations/osint_alarm_2025.kml",
                               "kml/Geolocations/osint_alarm_2026.kml"];
 window.attacksOnUaLayers = [];
+
 
 
 
