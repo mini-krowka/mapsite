965cc0b72bb86f42df7c08fb0d4cd2de32ee932c
mini-krowka <tophbeifong123@protonmail.com>
2026-02-26 22:03:43 +0300

    Изменен файл рвов.

--- js/data.js
+++ js/data.js
@@ -146,11 +146,13 @@ window.attacksOnUaLayers = [];
 window.fortificationKmlPaths = ["kml/Fortifications/Geojson/Trenches_Pokrovsk.geojson",
 								"kml/Fortifications/Geojson/Trenches_ Mezhevaya.geojson",
 								"kml/Fortifications/Geojson/Trenches_Kramatorsk.geojson",
-								"kml/Fortifications/Geojson/Ditches.geojson",
+								//"kml/Fortifications/Geojson/Ditches.geojson",
+								"kml/Fortifications/Geojson/ditches3.geojson",
 								"kml/Fortifications/Geojson/Barbed_wire.geojson",
                                 //"kml/Fortifications/Geojson/Dragons_teeth.geojson",
 							    "kml/Fortifications/Geojson/teeth.geojson"];
 window.fortificationLayers = [];
+
 
 
 
