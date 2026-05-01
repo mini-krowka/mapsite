0b6633cc450d45077e49ec99d0e095ee1427ed49
mini-krowka <tophbeifong123@protonmail.com>
2026-05-01 22:45:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -793,8 +793,8 @@ function parsePlacemarksFromKmlDoc(kmlDoc, styles, styleMaps, layerGroup, styleM
 	            weight: (this._originalStyle.weight || 0) + 3,
 	            color: '#ff0000',
 	            fillColor: '#ff0000',
-				opacity: 0.25,        // для линий
-				fillOpacity: 0.1     // для полигонов
+				opacity: 0.15,        // для линий
+				fillOpacity: 0.15     // для полигонов
 	        });
 	    });
 	    layer.on('mouseout', function() {
