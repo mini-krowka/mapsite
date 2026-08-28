7707c7002650133b339e33e624c2ba37305836b1
mini-krowka <224066524+mini-krowka@users.noreply.github.com>
2026-08-28 17:59:02 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -101,7 +101,7 @@ window.permanentLayers = [
 	
     // Контур максимального продвижения в Доброполье
      { name: "Добропольский прорыв",  path: "kml/PermanentObjects/Доброполье.kml" },
-	{ name: "Уверенный контроль Востока",  path: "kml/PermanentObjects/Восток_разделённые_геометрии.kml" },
+	{ name: "Уверенный контроль Востока",  path: "kml/PermanentObjects/Восток2.kml" },
 	
 	// Мультиполигон
     { name: "Мультиполигон",         path: "kml/PermanentObjects/CitiesBorders.kml" }
